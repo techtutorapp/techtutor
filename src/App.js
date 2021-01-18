586e923cd67458787d461ff41e8ea5b8845a081f
ecuber <37519236+ecuber@users.noreply.github.com>
2021-01-17 20:51:08 -0500

    day 1 development
    fixing reactstrap would be a good idea

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg'
 import './App.css'
+import NavbarHome from './Components/NavbarHome'
 
 function App () {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-           beep boop beep test site
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          docs
-        </a>
+      <header>
+        <NavbarHome></NavbarHome>
       </header>
     </div>
   )
